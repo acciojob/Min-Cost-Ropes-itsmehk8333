a002302263ab11663f333c9cbb160134ca2f3e8e
Harikrishna Gunnala <89335989+itsmehk8333@users.noreply.github.com>
2025-08-29 10:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function mincost(arr)
 { 
+
+	arr = arr.sort((a,b) => a-b);
+
+	var sum = 0
+const sumArr = arr.map((e) => {
+    
+    sum = sum+ e
+    return sum
+})
+
+ console.log(sumArr)
+ 
+ var totalSum = 0
+ for(var i =1 ; i<sumArr.length ;i++){
+     totalSum += sumArr[i] 
+ }
+
+
+	return totalSum
 //write your code here
 // return the min cost
   
